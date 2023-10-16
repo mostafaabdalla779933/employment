590cae6d88120d39cc64fde50d06afd1fe96e8fb
mostafa <mostafaabdalla779933@gmail.com>
2023-10-16 04:22:58 +0200

    handle user dashboard

--- app/src/main/java/com/employment/employment/feature/userDashboard/UserDashboardActivity.kt
+++ app/src/main/java/com/employment/employment/feature/userDashboard/UserDashboardActivity.kt
@@ -2,11 +2,44 @@ package com.employment.employment.feature.userDashboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.employment.employment.R
+import com.employment.employment.databinding.ActivityUserDashboardBinding
 
 class UserDashboardActivity : AppCompatActivity() {
+
+
+    lateinit var binding:ActivityUserDashboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_dashboard)
+        binding = ActivityUserDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initBottomNavigation()
+    }
+
+
+    private fun initBottomNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_user_activity_host) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+
+            when (destination.id) {
+                R.id.userHomeFragment, R.id.userMenuFragment,
+                R.id.userResumeFragment -> {
+                    binding.navView.visibility = View.VISIBLE
+                }
+                else -> {
+                    binding.navView.visibility = View.GONE
+
+                }
+
+            }
+        }
+        binding.navView.setOnItemReselectedListener { }
+
     }
 }
