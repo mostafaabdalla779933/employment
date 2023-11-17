35a9a11db1effc7dd9940e291dd4d6883c03a72b
mostafa <mostafaabdalla779933@gmail.com>
2023-11-17 14:12:58 +0200

    commit

--- app/src/main/java/com/employment/employment/common/firebase/data/UserModel.kt
+++ app/src/main/java/com/employment/employment/common/firebase/data/UserModel.kt
@@ -7,65 +7,100 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class UserModel(
-    var email:String? = "",
-    var password:String? ="",
-    var name:String? ="",
-    var firstName:String?="",
-    var lastName:String?="",
-    var userId :String? ="",
-    var uri: Uri?=null,
-    var profileUrl:String? ="",
+    var email: String? = "",
+    var password: String? = "",
+    var name: String? = "",
+    var firstName: String? = "",
+    var lastName: String? = "",
+    var userId: String? = "",
+    var uri: Uri? = null,
+    var profileUrl: String? = "",
     var mobile: String? = "",
-    var userType:String?="",
-    var listOfBranches:MutableList<BranchModel> = mutableListOf(),
-    var address: String?="",
-    var about:String?="",
-    var location:SelectedLocation?= null,
-    var companyWebsite:String?=""
+    var userType: String? = "",
+    var listOfBranches: MutableList<BranchModel> = mutableListOf(),
+    var address: String? = "",
+    var about: String? = "",
+    var location: SelectedLocation? = null,
+    var companyWebsite: String? = ""
 ) : Parcelable {
 
     fun getFullName() = "$firstName $lastName"
 }
 
 
-enum class UserType(val value:String){
-    User("User"),Company("Company")
+enum class UserType(val value: String) {
+    User("User"), Company("Company")
 }
 
-enum class UserState(val value:String){
-    Pending("Pending"),Accepted("Accepted"),Rejected("Rejected")
+val listOfQualifications = listOf(
+    "Less than high school",
+    "high school",
+    "bachelor's",
+    "master's",
+    "doctorate"
+)
+
+val listOfJobs = listOf(
+    "IT Programmer/Developer",
+    "Network Administrator",
+    "PC Support",
+    "Database Administrator",
+    "HR",
+    "Data Entry",
+    "Accountant",
+    "Arabic/English Translator"
+)
+
+val listOfResidencyTypes = listOf(
+    "Transferable",
+    "Tourist Visa",
+    "Non-Transferable"
+)
+
+
+val listOfAges = listOf(
+    "20 - 25",
+    "26 - 30",
+    "31 - 40",
+    "41 - 50",
+    "More than 50"
+)
+
+val listOfWorkTime = listOf(
+    "Full time",
+    "Part time"
+)
+
+enum class UserState(val value: String) {
+    Pending("Pending"), Accepted("Accepted"), Rejected("Rejected")
 }
 
 
 @Parcelize
 data class BranchModel(
-    var name:String?="",
-    var mobile:String?=""
+    var name: String? = "",
+    var mobile: String? = ""
 ) : Parcelable
 
 
 @Parcelize
 data class NotificationModel(
-    val message :String?=null,
-    val hash:String?=null,
-    val date:String?=null,
-    val fromId:String?=null,
-    var from: UserModel?=null,
+    val message: String? = null,
+    val hash: String? = null,
+    val date: String? = null,
+    val fromId: String? = null,
+    var from: UserModel? = null,
     var store: UserModel? = null,
-    val toUserId:String?=null,
-    val type:String?=null,
+    val toUserId: String? = null,
+    val type: String? = null,
 ) : Parcelable
-
-
-
-
 
 
 @Parcelize
 data class SelectedLocation(
-    var lat:Double?=null,
-    var long:Double?=null,
-    var address:String?=null
+    var lat: Double? = null,
+    var long: Double? = null,
+    var address: String? = null
 ) : Parcelable
 
 
