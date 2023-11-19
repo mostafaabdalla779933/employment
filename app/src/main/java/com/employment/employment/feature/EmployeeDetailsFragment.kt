70f81180ec8a6aaba3bd8efc10f643de2db0e2c8
Mina Ezzat <53598947+MeNa619493@users.noreply.github.com>
2023-11-19 23:41:46 +0200

    added job details

--- app/src/main/java/com/employment/employment/feature/EmployeeDetailsFragment.kt
+++ app/src/main/java/com/employment/employment/feature/EmployeeDetailsFragment.kt
@@ -1,12 +1,38 @@
 package com.employment.employment.feature
 
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.employment.employment.R
 import com.employment.employment.common.base.BaseFragment
 import com.employment.employment.databinding.FragmentEmployeeDetailsBinding
 
 class EmployeeDetailsFragment : BaseFragment<FragmentEmployeeDetailsBinding>() {
+
+    private val args: EmployeeDetailsFragmentArgs by navArgs()
+
     override fun initBinding() = FragmentEmployeeDetailsBinding.inflate(layoutInflater)
 
     override fun onFragmentCreated() {
+        binding.apply {
+            ivBack.setOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
+
+        displayData()
+    }
+
+    private fun displayData() {
+        binding.apply {
+            Glide.with(requireContext())
+                .load(args.employee.profileUrl)
+                .placeholder(R.drawable.ic_employee)
+                .into(ivEmployee)
+            tvEmployeeName.text = "${args.employee.firstName} ${args.employee.lastName}"
+            tvEmployeeNumber.text = args.employee.mobile
+            //tvEmployeeJob.text = args.employee.
+        }
     }
 
 }
